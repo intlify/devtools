4f972e60b78803c289da15b5522e8f7cd480f4b7
kazuya kawaguchi <kawakazu80@gmail.com>
2020-08-17 04:00:14 +0900

    design bridge module

--- packages/shared/src/bridge.ts
+++ packages/shared/src/bridge.ts
@@ -1,7 +1,64 @@
-export interface Bridge {}
+/* eslint-disable @typescript-eslint/ban-types */
 
-export interface Wall {}
+/**
+ * Event name
+ */
+export type Event = string
 
-export function createBridge(): Bridge {
-  return {}
+/**
+ * Event Message
+ */
+export type Message = string | string[] | { chunk: string; last: number }
+
+export type MessageHandler = (payload: Message) => void
+
+/**
+ * Bridge
+ */
+export interface Bridge {
+  send(event: Event, payload: Message): void
+  log(message: string): void
+  on(event: Event, handler: Function): void
+  off(event: Event, handler: Function): void
 }
+
+/**
+ * Wall
+ */
+export interface Wall {
+  listen(fn: MessageHandler): void
+  send(payload: Message): void
+}
+
+/**
+ * Create a bridge
+ *
+ * @param wall - A {@link Wall}
+ * @returns A bridge
+ */
+export function createBridge(wall: Wall): Bridge {
+  function send(event: Event, payload: Message): void {
+    // TODO:
+  }
+
+  function log(message: string): void {
+    // TODO:
+  }
+
+  function on(event: Event, handler: Function): void {
+    // TODO:
+  }
+
+  function off(event: Event, handler: Function): void {
+    // TODO:
+  }
+
+  return {
+    send,
+    log,
+    on,
+    off
+  }
+}
+
+/* eslint-enable @typescript-eslint/ban-types */
--- packages/shared/src/emitter.ts
+++ packages/shared/src/emitter.ts
@@ -1,5 +1,14 @@
-export interface Emitter {}
+/* eslint-disable @typescript-eslint/ban-types */
+
+export interface Emitter {
+  on(event: string, handler: Function): void
+}
 
 export function createEmitter(): Emitter {
-  return {}
+  function on(event: string, handler: Function): void {
+    // TODO:
+  }
+  return { on }
 }
+
+/* eslint-enable @typescript-eslint/ban-types */
