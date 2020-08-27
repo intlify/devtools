8421a1ebbf748c44bd32442caf9f5203bb55369d
kazuya kawaguchi <kawakazu80@gmail.com>
2020-08-28 02:53:09 +0900

    app-frontend: export codes

--- packages/app-frontend/src/index.ts
+++ packages/app-frontend/src/index.ts
@@ -1,1 +1,10 @@
-export {}
+import { Bridge } from '@intlify-devtools/shared'
+
+export interface Shell {
+  connect(fn: (bridge: Bridge) => void): void
+  onReload(fn: () => void): void
+}
+export function setupDevtools(shell: Shell): void {
+  // TODO:
+  console.log('setup Devtools!', shell)
+}
