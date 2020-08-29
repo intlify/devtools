b7f28244690e901c5f41a1896fefb632796219f3
kazuya kawaguchi <kawakazu80@gmail.com>
2020-08-30 02:47:24 +0900

    app-frontend: simple implementation

---
+++ packages/app-frontend/src/App.vue
@@ -0,0 +1,11 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App'
+})
+</script>
+
+<template>
+  <h1>Intliyf DevTools Panel</h1>
+</template>
--- packages/app-frontend/src/index.ts
+++ packages/app-frontend/src/index.ts
@@ -1,10 +1,26 @@
-import { Bridge } from '@intlify-devtools/shared'
+import { Bridge, isChrome } from '@intlify-devtools/shared'
+import { createApp, connectApp } from './app'
+
+const app = createApp()
+app.mount('#app')
 
 export interface Shell {
   connect(fn: (bridge: Bridge) => void): void
-  onReload(fn: () => void): void
+  onReload(fn: (bridge?: Bridge) => void): void
 }
+
 export function setupDevtools(shell: Shell): void {
-  // TODO:
   console.log('setup Devtools!', shell)
+
+  connectApp(app, shell)
+  shell.onReload(bridge => {
+    console.log('onReload!')
+
+    if (isChrome) {
+      app.config.errorHandler = undefined
+    }
+    bridge?.events.clear()
+
+    connectApp(app, shell)
+  })
 }
