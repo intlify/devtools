baeb951bad705b963c7757eff02b42fde13ea74a
kazuya kawaguchi <kawakazu80@gmail.com>
2020-09-23 14:54:46 +0900

    shell-dev: updates

--- packages/shell-dev/webpack.config.js
+++ packages/shell-dev/webpack.config.js
@@ -1,11 +1,8 @@
 const path = require('path')
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
-const { VueLoaderPlugin } = require('vue-loader')
+const { config } = require('@intlify-devtools/webpack-config')
 
-module.exports = (env = {}) => ({
-  mode: env.prod ? 'production' : 'development',
-  devtool: env.prod ? 'source-map' : 'cheap-module-eval-source-map',
-
+module.exports = config({
+  context: __dirname,
   entry: {
     main: './src/index.ts',
     hook: './src/hook.ts',
@@ -17,62 +14,6 @@ module.exports = (env = {}) => ({
     publicPath: '/build/',
     filename: '[name].js'
   },
-
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader'
-      },
-      {
-        test: /\.ts$/,
-        exclude: /node_modules/,
-        use: [
-          { loader: 'cache-loader' },
-          {
-            loader: 'thread-loader',
-            options: {
-              workers: require('os').cpus().length - 1,
-              poolTimeout: Infinity
-            }
-          },
-          {
-            loader: 'babel-loader'
-          },
-          {
-            loader: 'ts-loader',
-            options: {
-              happyPackMode: true,
-              transpileOnly: true,
-              appendTsSuffixTo: [/\.vue$/]
-            }
-          }
-        ]
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader'
-          }
-        ]
-      },
-      {
-        resourceQuery: /blockType=i18n/,
-        type: 'javascript/auto',
-        use: [
-          {
-            loader: '@intlify/vue-i18n-loader'
-            // options: {
-            //   preCompile: true
-            // }
-          }
-        ]
-      }
-    ]
-  },
-
   resolve: {
     extensions: ['.ts', '.js', '.vue'],
     alias: {
@@ -85,19 +26,8 @@ module.exports = (env = {}) => ({
         __dirname,
         '../app-backend/src'
       ),
-      vue: require.resolve('vue/dist/vue.esm-bundler.js')
+      vue: require.resolve('vue/dist/vue.esm-bundler.js'),
+      'vue-i18n': require.resolve('vue-i18n/dist/vue-i18n.esm-bundler.js')
     }
-  },
-
-  plugins: [
-    new VueLoaderPlugin(),
-    new ForkTsCheckerWebpackPlugin({
-      typescript: {
-        diagnosticOptions: {
-          semantic: true,
-          syntactic: true
-        }
-      }
-    })
-  ]
+  }
 })
