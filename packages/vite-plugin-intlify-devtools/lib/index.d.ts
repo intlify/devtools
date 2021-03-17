1974e4cebf6c15e39cbc4a0b2e11a15a38dcb823
kazuya kawaguchi <kawakazu80@gmail.com>
2021-03-17 19:05:11 +0900

    remove build files

--- packages/vite-plugin-intlify-devtools/lib/index.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Plugin } from 'vite';
-export declare type Options = {
-    devtools?: string;
-};
-declare function plugin(options?: Options): Plugin;
-export default plugin;
-export declare const intlify: typeof plugin;
